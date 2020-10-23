ff4e1ef5aa1aef890ef9ae084b9a0a6585de87e1
Dmitry Shamrilo <shamrilods@gmail.com>
2020-10-23 12:04:35 +0300

    refactoring code

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,4 @@ import './modules/polyfill'
 import './modules/fullpage/init-fullpage'
 import Paginator from './modules/fullpage/paginator'
 
-let paginator = new Paginator()
+const paginator = new Paginator()
